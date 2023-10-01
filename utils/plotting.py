6abfd1aa59cd8e71d2e0bf4fcd7b3398b2264220
NevVerVer <kuzmina7748@gmail.com>
2023-10-01 21:16:40 +0300

    fixed vector building functions

---
+++ utils/plotting.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.ndimage import gaussian_filter
+from scipy.ndimage import gaussian_filter1d
+from scipy import stats
+import math
+import scipy
+
+
+def open_file(fpath, show=False):
+    df = pd.read_csv(fpath, sep=" ", header = None, names=['numb', 'time', 'x', 'y'])
+    df = df.drop(df[df.time > 1755].index)
+    if show:
+        x = df['x'].values.tolist()
+        y = df['y'].values.tolist()
+        plot_tajectory(x, y)
+    return df
+
+def plot_tajectory(x, y):
+    time_cmap = np.arange(0, len(x))
+    plt.scatter(x, y, s=1, c=time_cmap)
+    plt.plot(x, y, c='gray', alpha=0.4)
+    plt.scatter(0, 0, marker='X', s=60, c='red')
+    plt.colorbar()
+    plt.show()
