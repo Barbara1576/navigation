fb90e8b354845bcaffa030f7a1278c51899d03a5
NevVerVer <kuzmina7748@gmail.com>
2023-11-02 13:42:17 +0300

    created new notebook for data preproc. working in delta angles notebook

--- utils/plotting.py
+++ utils/plotting.py
@@ -9,16 +9,6 @@ import math
 import scipy
 
 
-def open_file(fpath, show=False):
-    df = pd.read_csv(fpath, sep=" ", header = None, names=['numb', 'time', 'x', 'y'])
-    df = df.drop(df[df.time > 1755].index)
-    if show:
-        x = df['x'].values.tolist()
-        y = df['y'].values.tolist()
-        plot_trajectory(x, y)
-    return df
-
-
 def plot_trajectory(x, y):
     time_cmap = np.arange(0, len(x))
     plt.scatter(x, y, s=1, c=time_cmap)
@@ -26,3 +16,6 @@ def plot_trajectory(x, y):
     plt.scatter(0, 0, marker='X', s=60, c='red')
     plt.colorbar()
     plt.show()
+
+
+# TODO: add def for plot dots in areas 
--- utils/processing.py
+++ utils/processing.py
@@ -1,4 +1,15 @@
 import numpy as np
+import pandas as pd
+from utils.plotting import plot_trajectory
+
+
+def open_file(ff, show=True):
+    df = pd.read_csv(ff, sep=" ", header=None, names=['idx', 'time', 'x', 'y'])
+    df = df.drop('idx', axis=1)
+    df = df.drop(df[df.time > 1755].index)
+    if show:
+        plot_trajectory(df['x'], df['y'])
+    return df
 
 
 def normalize(y):
@@ -9,3 +20,15 @@ def normalize(y):
     if len(exceeded_indices) > 0:
         y[exceeded_indices] = np.fmod(y[exceeded_indices], 2*np.pi)
     return y
+
+
+def cut_jumps(df, xmax, xmin, ymax, ymin, show=True):
+    """
+    Simply drops samples with jumps onto the walls
+    """
+    dff = df[(df['x'] < xmax) & (df['x'] > xmin) & (df['y'] < ymax) & (df['y'] > ymin)]
+    ind = range(0, len(dff))
+    dff = dff.reindex(ind)
+    if show:
+        plot_trajectory(dff['x'], dff['y'])
+    return dff
